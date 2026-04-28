43647806e704d78cc1a77dcded2aa1f349776067
Shourjya Chakraborty <137634522+sc-likes-to-code@users.noreply.github.com>
2026-04-29 01:03:39 +0530

    Add example outputs for checksum error detection
    Added example outputs as comments to demonstrate functionality.

--- ass_28.4.26/checkSum_with_error_detection.c
+++ ass_28.4.26/checkSum_with_error_detection.c
@@ -91,3 +91,41 @@ int main() {
 
     return 0;
 }
+
+/*
+Output 1:
+
+Enter number of segments: 4
+Enter number of bits per segment: 4
+
+Enter the data segments (in decimal):
+Segment 1: 5
+Segment 2: 7
+Segment 3: 3
+Segment 4: 2
+
+Computed Checksum: 2
+
+Do you want to introduce error? (1 = Yes, 0 = No): 0
+
+No error detected in received data.
+
+Output 2:
+
+Enter number of segments: 4
+Enter number of bits per segment: 4
+
+Enter the data segments (in decimal):
+Segment 1: 5
+Segment 2: 7
+Segment 3: 3
+Segment 4: 2
+
+Computed Checksum: 2
+
+Do you want to introduce error? (1 = Yes, 0 = No): 1
+Enter segment position to alter (1 to 4): 2
+Enter new value for that segment: 6
+
+Error detected in received data!
+*/
